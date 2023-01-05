720c6073f0ea9b3c5357d3b80322161e61b074b6
viganzeqiri <viganzeqirii@gmail.com>
2023-01-05 11:29:21 +0100

    removed webhook related code

--- src/app.ts
+++ src/app.ts
@@ -2,24 +2,14 @@ import express, { Express } from "express";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
 
-import {
-  addAlchemyContextToRequest,
-  validateAlchemySignature,
-} from "./utils/alchemy-webhook";
-
 import * as pollsController from "./controllers/polls";
 
 dotenv.config();
-
-const signingKey = process.env.SIGNING_KEY;
 
 const app: Express = express();
 
 app.use(bodyParser.json());
 app.set("port", process.env.PORT || 3000);
-
-app.use("/pollsWebhook", express.json({ verify: addAlchemyContextToRequest }));
-app.use("/pollsWebhook", validateAlchemySignature(signingKey));
 
 app.get("/polls", pollsController.getPolls);
 app.post("/polls", pollsController.createPoll);
