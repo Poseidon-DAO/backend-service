5c7733acf458f41574539ee0779946af36a89606
viganzeqiri <viganzeqirii@gmail.com>
2023-03-13 06:49:51 +0100

    Changed the rules on the zod schema

--- src/types/artist.ts
+++ src/types/artist.ts
@@ -4,7 +4,7 @@ const ArtistSchema = z.object({
   name: z
     .string({ required_error: "The name field is required." })
     .trim()
-    .min(3, "The name field must contain at least 3 characters.")
+    .min(2, "The name field must contain at least 3 characters.")
     .max(70, "The name field must contain less than 70 characters."),
   email: z
     .string()
@@ -13,11 +13,9 @@ const ArtistSchema = z.object({
     .email({ message: "The email field has a wrong email." }),
   bio: z
     .string()
-    .min(10, "The bio field must contain at least 10 characters.")
     .max(1000, "The bio field must contain less than 1000 characters."),
   exhibitions: z
     .string()
-    .min(10, "The exhibitions field must contain at least 10 characters.")
     .max(1000, "The exhibitions field must contain less than 1000 characters.")
     .optional(),
   samples: z
@@ -26,19 +24,18 @@ const ArtistSchema = z.object({
     .optional(),
   twitter_url: z
     .string()
-    .url()
     .regex(
-      /^https?:\/\/(www\.)?twitter\.com\/[a-zA-Z0-9_]+\/?$/,
+      /^(https?:\/\/)?(www\.)?twitter\.com\/[a-zA-Z0-9_]+\/?$/,
       "The twitter_url field has a wrong url."
     ),
   instagram_url: z
     .string()
-    .url()
     .regex(
-      /^https?:\/\/(www\.)?instagram\.com\/[a-zA-Z0-9_\.]+\/?$/,
+      /^(https?:\/\/)?(www\.)?instagram\.com\/[a-zA-Z0-9_]+\/?$/,
       "The instagram_url field has a wrong url."
     )
-    .optional(),
+    .optional()
+    .or(z.literal("")),
   website: z
     .string()
     .url({ message: "The website field has a wrong url." })
