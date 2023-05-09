b622efd18e7cb795688daf74abcfc5e30e85bca0
viganzeqiri <viganzeqirii@gmail.com>
2023-05-09 15:00:00 +0200

    Added endpoint for voting and revoting

---
+++ src/controllers/user.ts
@@ -0,0 +1,48 @@
+import { type Request, type Response } from "express";
+import { ZodError } from "zod";
+
+import { prismaClient } from "../db-client";
+import { UserSchema } from "../types/user";
+
+/**
+ * @route POST /
+ */
+export const register = async (
+  req: Request<{}, {}, { address: string }>,
+  res: Response
+) => {
+  try {
+    const { address } = UserSchema.parse(req.body);
+
+    const user = await prismaClient.user.findUnique({
+      where: { address },
+    });
+
+    if (!!user) {
+      return res.status(409).json({
+        error: `User with address: <${address}> already exists!`,
+      });
+    }
+
+    const newUser = await prismaClient.user.create({
+      data: { address },
+    });
+
+    return res.json(newUser);
+  } catch (error) {
+    console.error(error);
+
+    if (error instanceof ZodError) {
+      const formattedErrors = error.flatten().fieldErrors;
+
+      const errorMessage = {
+        message: "Validation error",
+        errors: formattedErrors,
+      };
+
+      return res.status(400).json(errorMessage);
+    }
+
+    res.status(500).json({ error: "An unexpected error occurred" });
+  }
+};
--- src/db-client.ts
+++ src/db-client.ts
@@ -1,5 +1,20 @@
 import { PrismaClient } from "@prisma/client";
 
-const prismaClient = new PrismaClient();
+declare global {
+  var cachedPrisma: PrismaClient;
+}
+
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.cachedPrisma) {
+    global.cachedPrisma = new PrismaClient();
+  }
+  prisma = global.cachedPrisma;
+}
+
+const prismaClient = prisma;
 
 export { prismaClient };
